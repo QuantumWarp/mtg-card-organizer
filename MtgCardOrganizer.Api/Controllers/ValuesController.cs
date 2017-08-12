5189b11c03e2f2957f5431eb7016aa51bc658f5b
John Lowther <jw.lowther@hotmail.co.uk>
2017-08-12 01:40:43 +0100

    Added Angular and API. Not linked yet.

---
+++ MtgCardOrganizer.Api/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MtgCardOrganizer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
