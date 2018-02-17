b8fd5aea2ad69fd18de2283a656eb91c84d750a8
John Lowther <jw.lowther@hotmail.co.uk>
2018-02-17 11:59:44 +0000

    Login component improvements

--- mtg-card-organizer/src/app/authentication/authentication.module.ts
+++ mtg-card-organizer/src/app/authentication/authentication.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../general/shared.module';
+import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [
+    LoginComponent,
   ],
   imports: [
     SharedModule,
     RouterModule.forChild([
-      // { path: 'login', component: LoginComponent }
+      {
+        path: 'login',
+        component: LoginComponent,
+      }
     ]),
   ],
   providers: [
