7cb969bd077de9fad316fe29b92f987d9d05cbfe
John Lowther <jw.lowther@hotmail.co.uk>
2018-03-07 08:56:39 +0000

    More navbar, register page

--- mtg-card-organizer/src/app/general/layouts/standard-layout.component.ts
+++ mtg-card-organizer/src/app/general/layouts/standard-layout.component.ts
@@ -4,13 +4,6 @@ import { Component, trigger, state, style, transition, animate } from '@angular/
   selector: 'app-standard-layout',
   templateUrl: './standard-layout.component.html',
   styleUrls: ['./standard-layout.component.scss'],
-  animations: [
-    trigger('navToggle', [
-      state('0', style({})),
-      state('1', style({left: '0px'})),
-      transition('0 <=> 1', animate('100ms')),
-    ])
-  ]
 })
 export class StandardLayoutComponent {
 }
--- mtg-card-organizer/src/app/general/navigation/navigator.component.ts
+++ mtg-card-organizer/src/app/general/navigation/navigator.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, style, state, trigger, transition, animate } from '@angular/core';
 
 import { navModel } from './nav-model';
 
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
-  styleUrls: ['./navigation.scss']
+  styleUrls: ['./navigation.scss'],  
+  animations: [
+    trigger('navToggle', [
+      state('0', style({})),
+      state('1', style({width: '200px'})),
+      transition('0 <=> 1', animate('100ms')),
+    ])
+  ]
 })
 export class NavigatorComponent {
   open = false;
