b3eae956fe820edcb710f9444a97023f45c01e25
John Lowther <jw.lowther@hotmail.co.uk>
2019-02-22 23:05:42 +0000

    Remove unused ExpressionHelper

--- MtgCardOrganizer.Dal/Utilities/QueryableExtensions.cs
+++ MtgCardOrganizer.Dal/Utilities/QueryableExtensions.cs
@@ -11,12 +11,6 @@ namespace MtgCardOrganizer.Dal.Utilities
 {
     public static class PageSortFilterExtensions
     {
-        public static async Task<PagedData<T>> ApplyQueryModelAsync<T>(this IQueryable<T> queryable, QueryModel<T> queryModel, PropertySort<T> defaultSort = null) {
-            queryable = queryable.ApplyFilters(queryModel.Filters);
-            queryable = queryable.ApplySorting(queryModel.Sort, defaultSort);
-            return await queryable.ApplyPagingAsync(queryModel.Paging);
-        }
-
         public static async Task<PagedData<T>> ApplyPagingAsync<T>(this IQueryable<T> queryable, Paging paging)
         {
             paging = paging != null ? paging : new Paging() { Limit = 10 };
@@ -33,34 +27,12 @@ namespace MtgCardOrganizer.Dal.Utilities
             return query.ApplyQuery(queryable);
         }
 
-        private static IQueryable<T> ApplyFilters<T>(this IQueryable<T> queryable, IEnumerable<PropertyFilter<T>> filters) {
-            foreach (var filter in filters) {
-                queryable = queryable.ApplyFilter(filter);
-            }
-            return queryable;
-        }
-
-        private static IQueryable<T> ApplySorting<T>(this IQueryable<T> queryable, PropertySort<T> sort, PropertySort<T> defaultSort = null) {
-            if (sort == null && defaultSort == null) return queryable;
-            sort = sort != null ? sort : defaultSort;
-            if (sort.Ascending) {
-                return queryable.OrderBy(ExpressionHelper.CreateKeySelectorExpression<T>(sort));
-            } else {
-                return queryable.OrderByDescending(ExpressionHelper.CreateKeySelectorExpression<T>(sort));
-            }
-        }
-
         private static IQueryable<T> ApplyOffset<T>(this IQueryable<T> queryable, int offset) {
             return queryable.Skip(offset);
         }
         
         private static IQueryable<T> ApplyLimit<T>(this IQueryable<T> queryable, int? limit) {
             return limit.HasValue ? queryable.Take(limit.Value) : queryable;
-        }
-
-        private static IQueryable<T> ApplyFilter<T>(this IQueryable<T> queryable, PropertyFilter<T> filter) {
-            var expression = ExpressionHelper.CreateFilterExpression<T>(filter);
-            return queryable.Where(expression);
         }
 
         public static IQueryable<T> ConditionalWhere<T>(this IQueryable<T> queryable, Expression<Func<T, bool>> predicate, bool condition)
